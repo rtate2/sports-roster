b1feda21e78bc98bcac757a0bff61d87a87fa495
Randy Tate <randytatejr@gmail.com>
2019-12-25 08:07:03 -0600

    completed auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import firebase from 'firebase/app';
+import firebaseConnection from '../helpers/Data/connection';
+import Auth from '../components/Auth/auth';
+import MyNavbar from '../components/MyNavbar/myNavbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+import './App.scss';
+
+firebaseConnection.firebaseApp();
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const { authed } = this.state;
+
+    return (
+      <div className="App">
+        <MyNavbar authed={authed} />
         <button className="btn btn-success">Nice Button</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        {
+          (authed) ? (<div>You Logged In</div>) : (<Auth />)
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
